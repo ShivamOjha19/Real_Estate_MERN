bfa53fc3d4b6f414bfc0de9e455fedaa93e6e3f9
Nova2419 <shivamojha2419@gmail.com>
2024-01-03 05:48:24 +0530

    Bugs Fixed

--- api/index.js
+++ api/index.js
@@ -1,4 +1,21 @@
+import dotenv from 'dotenv';
+
 import express from 'express'
+import mongoose from 'mongoose'
+
+dotenv.config();
+
+console.log("MongoDB Connection String:", process.env.MONGODB_URI);
+
+mongoose
+.connect(process.env.MONGODB_URI)
+.then( () => {
+ console.log("Connected to MongoDB");
+})
+.catch((err) => {
+ console.log(err)
+})
+
 
 const app = express();
 
