998ff04b07b611ce3ad73eaf52bbeb27eb3aba78
Nova2419 <shivamojha2419@gmail.com>
2024-01-08 02:36:17 +0530

    Issues Fixed

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -51,7 +51,7 @@ export default function SignIn() {
   return (
     <div className='p-3 mx-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>SignIn</h1>
-      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+      <form className='flex flex-col gap-4 items-center' onSubmit={handleSubmit}>
         <input type="text" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
         <input type="text" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
         <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -51,7 +51,7 @@ export default function SignUp() {
   return (
     <div className='p-3 mx-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>SignUp</h1>
-      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+      <form className='flex flex-col gap-4 items-center' onSubmit={handleSubmit}>
         <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange} />
         <input type="text" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
         <input type="text" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
