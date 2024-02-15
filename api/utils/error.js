efd2edf17596da52b701ca4458d9d4a70d453d02
Nova2419 <shivamojha2419@gmail.com>
2024-02-15 13:05:32 +0530

    Buggs Fixed

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,8 +1,6 @@
-const errorHandler = (statusCodee, message) => {
- const error = new Error()
- error.statusCode = statusCodee
- error.message = message
- return error
-}
-
-export default errorHandler;
+export const errorHandler = (statusCode, message) => {
+  const error = new Error();
+  error.statusCode = statusCode;
+  error.message = message;
+  return error;
+};
