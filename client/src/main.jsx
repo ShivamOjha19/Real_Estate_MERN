0c0eed0f9e33dd1b9b9327dd5a077bfb2cb26ba8
Nova2419 <shivamojha2419@gmail.com>
2024-02-15 13:20:29 +0530

    Final bugs fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,11 @@
-//import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { persistor, store } from './redux/store.js'
-import{ Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { persistor, store } from './redux/store.js';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
@@ -13,4 +13,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
     </PersistGate>
   </Provider>
-  )
+);
